c98157e9177732ebee97aefe3b753ae24473ace2
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-05-09 23:51:58 +0530

    feat: add home endpoint to routers.py with welcome message

--- backend/app/api/routers.py
+++ backend/app/api/routers.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+
+@router.get()
+def home():
+    return {
+        "message": "welcome to the alpha zen bank, where security and technology shake hands"
+    }

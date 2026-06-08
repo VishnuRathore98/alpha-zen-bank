f72dab1d1a300a111474e8ad0cb68c4ee10ee16f
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-06-08 23:09:54 +0530

    feat: add method to retrieve descriptions for security questions

--- backend/app/auth/schema.py
+++ backend/app/auth/schema.py
@@ -9,3 +9,14 @@ class SecurityQuestionsSchema(str, Enum):
     CHILDHOOD_FRIEND = "childhood_friend"
     FAVORITE_COLOR = "favorite_color"
     BIRTH_CITY = "birth_city"
+
+    @classmethod
+    def get_description(cls, value: "SecurityQuestionsSchema") -> str:
+        descriptions = {
+            cls.MOTHER_MAIDEN_NAME: "What is the name of your mother?",
+            cls.CHILDHOOD_FRIEND: "What is the name of your childhood friend?",
+            cls.FAVORITE_COLOR: "What is your favorite color?",
+            cls.BIRTH_CITY: "What is the name of the city you were born in?",
+        }
+
+        return descriptions.get(value, "Unknown security question")

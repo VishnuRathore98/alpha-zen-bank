c3fac69ed6a9dc929f4526bf4a8aed0b79ea47ca
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-07-01 23:04:47 +0530

    feat: add user schemas for reading and login requests, and update dependencies for pyjwt

--- backend/app/auth/schema.py
+++ backend/app/auth/schema.py
@@ -69,3 +69,28 @@ class UserCreateSchema(BaseUserSchema):
                 },
             )
         return v
+
+
+class UserReadSchema(BaseUserSchema):
+    id: uuid.UUID
+    full_name: str
+
+
+class EmailRequestSchema(SQLModel):
+    email: EmailStr
+
+
+class LoginRequestSchema(SQLModel):
+    email: EmailStr
+    password: str = Field(
+        min_length=8,
+        max_length=40,
+    )
+
+
+class OTPVerifyRequestSchema(SQLModel):
+    email: EmailStr
+    otp: str = Field(
+        min_length=6,
+        max_length=6,
+    )

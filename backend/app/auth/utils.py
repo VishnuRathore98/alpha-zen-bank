8e6c925abb05f389fe1b3cf352679d88652917df
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-07-05 23:48:06 +0530

    feat: add user activation email config and service

--- backend/app/auth/utils.py
+++ backend/app/auth/utils.py
@@ -40,4 +40,13 @@ def generate_username() -> str:
 
 
 def create_activation_token(id: uuid.UUID) -> str:
-    pass
+    payload = {
+        "id": str(id),
+        "type": "activation",
+        "exp": datetime.now(timezone.utc)
+        + timedelta(minutes=settings.ACTIVATION_TOKEN_EXPIRATION_MINUTES),
+        "iat": datetime.now(timezone.utc),
+    }
+    return jwt.encode(
+        payload, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+    )
